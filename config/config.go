4f6e7e68eba6810982de2df3d1533fb9ec454976
Arif Rahman <arif.rahman.rhm@gmail.com>
2025-01-22 10:56:17 +0700

    chore: customize app host

--- config/config.go
+++ config/config.go
@@ -1,37 +1,40 @@
 package config
 
 import (
-    "github.com/joho/godotenv"
-    "os"
-    "strings"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 var (
-    AppPort       string
-    UploadPath    string
-    LogPath       string
-    AllowedOrigins []string
+	AppPort        string
+	AppHost        string
+	UploadPath     string
+	LogPath        string
+	AllowedOrigins []string
 )
 
 func init() {
-    // Load environment variables from .env file
-    if err := godotenv.Load(); err != nil {
-        panic("Error loading .env file")
-    }
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		panic("Error loading .env file")
+	}
 
-    // Initialize configuration variables
-    AppPort = getEnv("APP_PORT", "8001")
-    UploadPath = getEnv("UPLOAD_PATH", "./uploads")
-    LogPath = getEnv("LOG_PATH", "./logs")
-    
-    // Load allowed origins from environment variable
-    AllowedOrigins = strings.Split(getEnv("ALLOWED_ORIGINS", "*"), ",")
+	// Initialize configuration variables
+	AppPort = getEnv("APP_PORT", "8001")
+	AppHost = getEnv("APP_HOST", "127.0.0.1")
+	UploadPath = getEnv("UPLOAD_PATH", "./uploads")
+	LogPath = getEnv("LOG_PATH", "./logs")
+
+	// Load allowed origins from environment variable
+	AllowedOrigins = strings.Split(getEnv("ALLOWED_ORIGINS", "*"), ",")
 }
 
 // getEnv retrieves an environment variable or returns a default value
 func getEnv(key, defaultValue string) string {
-    if value, exists := os.LookupEnv(key); exists {
-        return value
-    }
-    return defaultValue
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
